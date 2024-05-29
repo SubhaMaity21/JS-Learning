1dafa007192911cea06b7ee12252130b43016e74
Subha Maity <maitysubha1321@gmail.com>
2024-05-29 10:35:45 +0530

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 23454
+let accountEmail ="subha@gmail.com"
+var accountPassword ="rfthgg" 
+accountCity ="Kolkata" //don't use this method
+accountCity ="Delhi"
+
+let accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+//alert (3+3) //we are using node js ,not browser
+console.log(3
+    +3
+)  // this is very bad,code readability should be high
+
+//number => 2 to the power of 53
+//bigint
+//string => ""
+//boolean > true/false
+// null > stand alone value
+//undefined > value is not yet assigned
+//symbol > unique
+
+
+//object
+
+console.log(typeof null)
+console.log(typeof undefined);
