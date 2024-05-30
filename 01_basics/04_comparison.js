bbc19daf3e8371a2ce269d18de8d10557f418c50
Subha Maity <maitysubha1321@gmail.com>
2024-05-30 10:39:16 +0530

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+console.log("2" >1);
+// don't compare same data type
+console.log(null>=0);
+console.log(null >0);
+console.log(null == 0); //avoid this type of comparison
+// when there is a comparisopn js converts null 
+// into zero ,but when it's == it will notcovert nulli into zero
+
+
+console.log("2"===2)
+// its false as === mens strictly
+
+console.log(undefined ==0);
+console.log(undefined >0);
+console.log(undefined >=0);
