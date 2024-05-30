7875b2fe34f3ac4220540af2f37c442c4ae7adb0
Subha Maity <maitysubha1321@gmail.com>
2024-05-30 11:24:26 +0530

    datatypes summery

---
+++ 01_basics/05_datatypesSummery.js
@@ -0,0 +1,27 @@
+// #primitive datatypes (call by value)
+
+// 7 types - String, Number, Boolean,null,undefined,Symol,BigInt,
+let bigNumer = 100n
+
+
+
+
+// ReferenceType(non-primitie)
+
+// array,objects,functions
+
+const heros =["shaktiman","doga"]
+
+let myObj ={
+    name: "Subha",
+    age: 18
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof myObj);
+console.log(typeof heros);
+console.log(typeof myFunction);
+// acually it's function object
