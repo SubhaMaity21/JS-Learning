28237cf5bec277534d1904e9161f8fbce54bee75
Subha Maity <maitysubha1321@gmail.com>
2024-06-03 17:41:40 +0530

    Number and mathematics

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,39 @@
+const score = 600
+const balance =new Number(100)
+
+console.log(balance);
+console.log(score);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 299.8636
+
+console.log(otherNumber.toPrecision(3));
+
+const newNumber = 1000000
+console.log(newNumber.toLocaleString('en-IN'));//converted into indian ecosystem
+
+
+//********************************  Maths  ******************************** 
+
+
+console.log(Math.abs(-4));//negative to positive
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.3));
+
+console.log(Math.pow(2,3));
+console.log(Math.sin(30)); //it takes the value in radian
+
+
+
+
+console.log(Math.random());//it gives a value between 0 ans 1 including 0 excluding 1
+
+const min = 1
+const max = 6
+
+// console.log(Math.floor(Math.random()*(max-min +1)+min));
+
+
