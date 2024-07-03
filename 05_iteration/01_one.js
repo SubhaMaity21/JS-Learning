62e24dc03989b2e2a7d9d9d07eec30974de010bc
Subha Maity <maitysubha1321@gmail.com>
2024-07-03 10:40:27 +0530

    for loop

---
+++ 05_iteration/01_one.js
@@ -0,0 +1,40 @@
+//for loop
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if(element ==5){
+        // console.log("it's best");
+    }
+    // console.log(element);
+}
+
+
+
+
+for (let i = 1; i <= 10; i++) {
+//  console.log(`outer value${i}`);
+    for (let j = 1; j <= 10; j++) {
+        
+    //    console.log(`inner loop value ${j} and inner loop ${i}`);
+    // console.log(i + '*'+ j + '=' + i*j);
+    }
+}
+
+
+
+let myArr =["flash","batman","Superman","Shaktiman"]
+
+for (let i = 0; i < myArr.length; i) {
+    const element = myArr[i];
+    // console.log(element);
+}
+
+
+
+// break and continue
+
+
+for (let index = 1; index < 20; index++) {
+    console.log(`value of i is  ${index}`);
+    
+}
